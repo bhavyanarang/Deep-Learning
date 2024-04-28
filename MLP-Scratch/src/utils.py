d81a3ba836dd025b93f487b0dcafe84c529e1b95
Bhavya Narang <bnarang@atlassian.com>
2024-04-28 19:51:30 +0530

    created folder structure and modularized code

---
+++ MLP-Scratch/src/utils.py
@@ -0,0 +1,10 @@
+import os
+
+def check_folder_exists(folder_path):
+    
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        print(f"Folder '{folder_path}' created successfully.")
+    else:
+        print(f"Folder '{folder_path}' already exists.")
+        
